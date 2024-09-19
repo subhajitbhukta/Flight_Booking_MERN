965af732c9cd8821e4391b730671aecc68b73f0b
subhajitbhukta <143212971+subhajitbhukta@users.noreply.github.com>
2024-09-19 22:35:35 +0530

    Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
       dispatch(setSearchParams({ from, to, departureDate, tripType }));
 
       // Fetch flights data from the API
-      const response = await axios.get('https://jetquestsubhajit.netlify.app/api/flights');
+      const response = await axios.get('https://flight-backend-hc4f.onrender.com/api/flights');
 
 
       // Dispatch the fetched flights to the Redux store
